1efd34fff5362f60ae204adb9c17205be4ff7ce8
jalemy <jalemy.xps@gmail.com>
2019-05-20 08:50:32 +0900

    変数名変更

--- main.js
+++ main.js
@@ -1,46 +1,46 @@
 function main() {
   var sheet = SpreadsheetApp.getActiveSheet();
 
-  var values = sheet.getRange('A:A').getValues().filter(String);
+  var members = sheet.getRange('A:A').getValues().filter(String);
+  var days = sheet.getRange(1, 2).getValue();
+  var limit = Math.round(days * 5 / members.length);
 
-  var count = sheet.getRange(1, 2).getValue();
-
-  if (count > values.length) {
+  if (days > members.length) {
     Browser.msgBox('人数が足りません');
     return;
   }
 
-  var result = lottery(values, count);
+  var result = lottery(members, days);
   sheet.getRange('E:E').clearContent();
   sheet.getRange(1, 5).setValue('月曜日');
   sheet.getRange(2, 5, result.length, 1).setValues(result);
 
-  result = lottery(values, count);
+  result = lottery(members, days);
   sheet.getRange('F:F').clearContent();
   sheet.getRange(1, 6).setValue('火曜日');
   sheet.getRange(2, 6, result.length, 1).setValues(result);
 
-  result = lottery(values, count);
+  result = lottery(members, days);
   sheet.getRange('G:G').clearContent();
   sheet.getRange(1, 7).setValue('水曜日');
   sheet.getRange(2, 7, result.length, 1).setValues(result);
 
-  result = lottery(values, count);
+  result = lottery(members, days);
   sheet.getRange('H:H').clearContent();
   sheet.getRange(1, 8).setValue('木曜日');
   sheet.getRange(2, 8, result.length, 1).setValues(result);
 
-  result = lottery(values, count);
+  result = lottery(members, days);
   sheet.getRange('I:I').clearContent();
   sheet.getRange(1, 9).setValue('金曜日');
   sheet.getRange(2, 9, result.length, 1).setValues(result);
 }
 
-function lottery(values, count) {
+function lottery(members, days) {
   var result = [];
 
-  for (var i = 0; i < count; i++) {
-    var value = values[Math.floor(Math.random() * values.length)];
+  for (var i = 0; i < days; i++) {
+    var value = members[Math.floor(Math.random() * members.length)];
 
     var array = Array.prototype.concat.apply([], result);
     if (array.indexOf(value) !== -1) {
